7b05ce2f40964727e73da41e64833327399c36e0
gaopengfei <gaopengfei@qianxin.com>
2023-08-09 16:11:38 +0800

    support config.json and envir flag

--- cmd/root.go
+++ cmd/root.go
@@ -2,11 +2,15 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"log"
+	"os"
 	"runtime"
+	"strings"
+	"vanity-generator/cmd/args"
 )
 
-var prefix, suffix string // 前缀、后缀匹配
-var concurrency int32     // 并发控制
+var arg args.Args
 
 var rootCmd = &cobra.Command{
 	Use:   "vanity-generator",
@@ -19,8 +23,54 @@ func Execute() {
 }
 
 func init() {
+	initCommand()
+	initConfig()
+}
+
+func initCommand() {
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
-	rootCmd.PersistentFlags().StringVar(&prefix, "prefix", "", "address prefix")
-	rootCmd.PersistentFlags().StringVar(&suffix, "suffix", "", "address suffix")
-	rootCmd.PersistentFlags().Int32Var(&concurrency, "concurrency", int32(runtime.NumCPU()), "concurrency limit")
+	rootCmd.PersistentFlags().StringVar(&arg.Prefix, "prefix", "", "address prefix")
+	rootCmd.PersistentFlags().StringVar(&arg.Suffix, "suffix", "", "address suffix")
+	rootCmd.PersistentFlags().Int32Var(&arg.Concurrency, "concurrency", int32(runtime.NumCPU()), "concurrency limit")
+}
+
+func initConfig() {
+	// file
+	viper.SetConfigName("config")
+	viper.AddConfigPath("/etc/vanity")
+	viper.SetConfigType("json")
+	viper.AllowEmptyEnv(true)
+	if err := viper.ReadInConfig(); err != nil {
+		if !strings.Contains(err.Error(), "Not Found") {
+			log.Fatalf("read config.json error: %+v", err)
+		}
+	}
+
+	// env
+	viper.AutomaticEnv()
+
+	// flag
+	_ = viper.BindPFlag("prefix", rootCmd.Flag("prefix"))
+	_ = viper.BindPFlag("suffix", rootCmd.Flag("suffix"))
+	_ = viper.BindPFlag("concurrency", rootCmd.Flag("concurrency"))
+
+	if len(os.Args) == 1 && viper.GetString("wallet") != "" {
+		os.Args = append(os.Args, viper.GetString("wallet"))
+	}
+}
+
+func addCommand(name, desc string, checkFn func(args.Args) error, runFn func(args.Args)) {
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   name,
+		Short: desc,
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			arg.Prefix = viper.GetString("prefix")
+			arg.Suffix = viper.GetString("suffix")
+			arg.Concurrency = viper.GetInt32("concurrency")
+			return checkFn(arg)
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			runFn(arg)
+		},
+	})
 }
--- runtime/runtime.go
+++ runtime/runtime.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"time"
+	"vanity-generator/cmd/args"
 	"vanity-generator/common"
 	"vanity-generator/cryptos"
 	"vanity-generator/cryptos/ethereum"
@@ -14,17 +15,16 @@ import (
 )
 
 type Executor struct {
-	prefix, suffix string
-	concurrency    int32
-	diff           float64
-	generator      cryptos.Generator
+	args      args.Args
+	diff      float64
+	generator cryptos.Generator
 
 	ctx           *context.Context
 	slidingWindow *sliding.SlidingWindow
 	wallet        *model.Wallet
 }
 
-func NewExecutor(symbol, prefix, suffix string, concurrency int32) *Executor {
+func NewExecutor(symbol string, args args.Args) *Executor {
 	var generator cryptos.Generator
 	switch symbol {
 	case "eth":
@@ -36,10 +36,8 @@ func NewExecutor(symbol, prefix, suffix string, concurrency int32) *Executor {
 	}
 
 	return &Executor{
-		prefix:        prefix,
-		suffix:        suffix,
-		concurrency:   concurrency,
-		diff:          float64(generator.Difficulty(prefix, suffix)),
+		args:          args,
+		diff:          float64(generator.Difficulty(args.Prefix, args.Suffix)),
 		ctx:           context.NewContext(),
 		generator:     generator,
 		slidingWindow: sliding.NewSlidingWindow(30),
@@ -47,7 +45,7 @@ func NewExecutor(symbol, prefix, suffix string, concurrency int32) *Executor {
 }
 
 func (e *Executor) Start() *model.Wallet {
-	for i := int32(0); i < e.concurrency; i++ {
+	for i := int32(0); i < e.args.Concurrency; i++ {
 		go e.exec()
 	}
 
@@ -61,7 +59,7 @@ func (e *Executor) Start() *model.Wallet {
 
 func (e *Executor) exec() {
 	for !e.ctx.IsDone() {
-		if wallet := e.generator.DoSingle(e.prefix, e.suffix); wallet != nil {
+		if wallet := e.generator.DoSingle(e.args.Prefix, e.args.Suffix); wallet != nil {
 			e.ctx.Finish()
 			e.wallet = wallet
 		}
@@ -82,7 +80,7 @@ func (e *Executor) ShowProgress() {
 	start := time.Now()
 	time.Sleep(time.Second + time.Millisecond*100)
 	for {
-		template := "duration: %v, hashing power: %s/s, expect p50: %-10v  p70: %-10v  p90: %-10v"
+		template := "duration: %v, hashing power: %s/s, expect 50%%: %-10v  70%%: %-10v  90%%: %-10v"
 		duration := time.Now().Sub(start).Round(time.Second)
 		power := common.HashPower(e.slidingWindow.Average())
 		info := fmt.Sprintf(template, duration, power, e.P50(), e.P70(), e.P90())
